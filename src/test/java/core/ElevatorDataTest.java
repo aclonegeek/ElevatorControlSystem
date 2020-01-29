c4a6c64115f15bbe6ffc233b44cb1dee32aa5a54
LayneKM <laynemikan@gmail.com>
2020-01-28 20:29:04 -0500

    refactor: add periods to comments at the behest of randy bobandy

--- src/test/java/core/ElevatorDataTest.java
+++ src/test/java/core/ElevatorDataTest.java
@@ -10,19 +10,19 @@ import java.time.LocalTime;
 import junit.framework.TestCase;
 
 public class ElevatorDataTest extends TestCase {
-    // Test writing/reading ElevatorData to/from a file
+    // Test writing/reading ElevatorData to/from a file.
     public void testFileSerialization() {
         final LocalTime time = LocalTime.now();
         final ElevatorData outputData = new ElevatorData(1, 2, 3, time);
         try {
-            // Serialize ElevatorData to file
+            // Serialize ElevatorData to file.
             final FileOutputStream fout = new FileOutputStream("elevatorDataTest.txt");
             final ObjectOutputStream out = new ObjectOutputStream(fout);
             out.writeObject(outputData);
             out.flush();
             out.close();
 
-            // Deserialize ElevatorData from file
+            // Deserialize ElevatorData from file.
             final ObjectInputStream in = new ObjectInputStream(new FileInputStream("elevatorDataTest.txt"));
             final ElevatorData inputData = (ElevatorData) in.readObject();
             assertEquals(outputData.getElevatorId(), inputData.getElevatorId());
@@ -31,13 +31,13 @@ public class ElevatorDataTest extends TestCase {
             assertEquals(outputData.getTime(), inputData.getTime());
             in.close();
 
-            // Delete file
+            // Delete file.
             final File file = new File("elevatorDataTest.txt");
             file.delete();
         } catch (Exception e) {
             System.err.println(e);
 
-            // Force a test failure if fall into catch block
+            // Force a test failure if fall into catch block.
             assertTrue(false);
         }
     }
