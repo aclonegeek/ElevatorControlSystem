c5eebfb8eb6fdbf9ccdb7dfb514ebc014f0a515e
Michal Debicki <michaldebicki@Michals-MacBook-Pro.local>
2020-01-30 18:31:12 -0500

    t 1

--- src/main/java/core/ElevatorSubsystem.java
+++ src/main/java/core/ElevatorSubsystem.java
@@ -1,4 +1,35 @@
 package core;
 
-public class ElevatorSubsystem {
+public class ElevatorSubsystem implements Runnable {
+
+    private final ElevatorData data;
+
+    public ElevatorSubsystem(ElevatorData dataInput){
+        this.data = dataInput;
+    }
+    public synchronized void run(){
+        while(true){
+
+            if(floorSubsystem.floorRequests(data) != null){
+                schedulerSubsystem.sendFloorRequest(data, floorSubsystem.requests(data));
+                //turn on the button light
+            }
+
+            if(schedulerSubsystem.floorRequests(data) != null){
+                if(schedulerSubsystem.floorRequests(data) != data.getCurrentFloor()){
+                    schedulerSubsystem.moveElevatorMotor(data);
+                    data.setCurrentFloor() = schedulerSubsystem.floorRequests(data);
+                    schedulerSubsystem.openElevatorDoor(data);
+                    schedulerSubsystem.closeElevatorDoor(data);
+                }
+            }
+
+            try{
+                Thread.sleep(1000);
+            }catch(Exception e){
+                System.out.println("Elevator " + data.getElevatorId() + " has woken up");
+                return;
+            }
+        }
+    }
 }
