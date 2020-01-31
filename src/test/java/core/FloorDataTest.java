db9203c63c00713f12dba5f2cbcc93e580b963e6
Galen <goshea@me.com>
2020-01-31 11:10:50 -0500

    test: added junit tests for floor data class

---
+++ src/test/java/core/FloorDataTest.java
@@ -0,0 +1,46 @@
+package core;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalTime;
+
+import core.FloorData.ButtonState;
+import junit.framework.TestCase;
+
+public class FloorDataTest extends TestCase {
+    
+    // Test writing/reading FloorData to/from a file.
+    public void testFileSerialization() {
+        final LocalTime time = LocalTime.now();
+        final FloorData outputData = new FloorData(1, 2, ButtonState.UP, time);
+        try {
+            // Serialize FloorData to file.
+            final FileOutputStream fout = new FileOutputStream("floorDataTest.txt");
+            final ObjectOutputStream out = new ObjectOutputStream(fout);
+            out.writeObject(outputData);
+            out.flush();
+            out.close();
+
+            // Deserialize FloorData from file.
+            final ObjectInputStream in = new ObjectInputStream(new FileInputStream("floorDataTest.txt"));
+            final FloorData inputData = (FloorData) in.readObject();
+            assertEquals(outputData.getElevatorId(), inputData.getElevatorId());
+            assertEquals(outputData.getFloorNumber(), inputData.getFloorNumber());
+            assertEquals(outputData.getButtonState(), inputData.getButtonState());
+            assertEquals(outputData.getTime(), inputData.getTime());
+            in.close();
+
+            // Delete file.
+            final File file = new File("floorDataTest.txt");
+            file.delete();
+        } catch (Exception e) {
+            System.err.println(e);
+
+            // Force a test failure if fall into catch block.
+            assertTrue(false);
+        }
+    }
+}
