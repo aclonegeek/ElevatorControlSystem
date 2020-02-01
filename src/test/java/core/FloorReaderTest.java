d91230a893b28822cd28439442dc9ad07cc734e6
Tan Tran <tantran@Tans-Macbook.local>
2020-02-01 17:38:12 -0500

    refactor: changes based on code reviews

--- src/test/java/core/FloorReaderTest.java
+++ src/test/java/core/FloorReaderTest.java
@@ -5,22 +5,24 @@ import core.FloorData.ButtonState;
 import junit.framework.TestCase;
 
 /**
- * @author Group 8 (implementation by Tan Tran)
- * 
  * @summary Iteration 1 - Tester to determine if FloorReader is working correclty
- * 
  */
 
 public class FloorReaderTest extends TestCase {
 
     public void testFloorReader() {
-        FloorReader floorReader = new FloorReader("./FloorData.txt");
+        final FloorReader floorReader = new FloorReader("./FloorData.txt");
+        FloorData floorData = floorReader.getFloorRequests().get(0);
         
-        FloorData floorData1 = floorReader.getFloorRequests().get(0);
+        assertEquals(1, floorData.getElevatorId());
+        assertEquals(2, floorData.getFloorNumber());
+        assertEquals(ButtonState.UP,floorData.getButtonState());
+        assertEquals(LocalTime.parse("14:05:15.500"),floorData.getTime());
         
-        assertEquals(1 , floorData1.getElevatorId());
-        assertEquals(2 , floorData1.getFloorNumber());
-        assertEquals(ButtonState.UP ,floorData1.getButtonState());
-        assertEquals(LocalTime.parse("14:05:15.500") ,floorData1.getTime());
+        floorData = floorReader.getFloorRequests().get(1);
+        assertEquals(1, floorData.getElevatorId());
+        assertEquals(8, floorData.getFloorNumber());
+        assertEquals(ButtonState.DOWN,floorData.getButtonState());
+        assertEquals(LocalTime.parse("18:25:35.600"),floorData.getTime()); 
     }
 }
