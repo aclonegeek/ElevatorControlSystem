6b4e1cc1cf6b800db0840a41f3c78755a6c38f11
Galen <goshea@me.com>
2020-02-01 14:05:11 -0500

    [WIP] added floor data reader code

--- src/main/java/core/FloorSubsystem.java
+++ src/main/java/core/FloorSubsystem.java
@@ -14,12 +14,13 @@ public class FloorSubsystem implements Runnable {
         UP, DOWN, IDLE, NONE
     }
 
+    private Scheduler scheduler;
     private ButtonState buttonState;
     private ElevatorState elevatorState;
     private final int number;
 
-    public FloorSubsystem(final int number) {
-        // this.scheduler = scheduler;
+    public FloorSubsystem(final Scheduler scheduler, final int number) {
+        this.scheduler = scheduler;
         this.buttonState = ButtonState.UNPRESSED;
         this.elevatorState = ElevatorState.NONE;
         this.number = number;
@@ -49,10 +50,15 @@ public class FloorSubsystem implements Runnable {
     // Run
     @Override
     public void run() {
+        this.scheduler.registerFloorSubsystem(this);
+        //ArrayList<FloorData> floorRequests = FloorReader(this.path).getFloorRequests();
         boolean running = true;
         while (running) {
-            //Need to implement 
-            // schedule = scheduler.getSchedule();
+            /*FloorData firstFloorRequest = floorRequests.get(0);
+            if(simTime == firstFloorRequest.getTime()){
+                floorRequests.remove(0);
+                this.scheduler.addFloorEvent(floorData);
+            }*/
         }
     }
 
