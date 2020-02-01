59380d1a856df555e01058fd2b9fa1b4d9ec8711
Galen <goshea@me.com>
2020-02-01 14:47:10 -0500

    refactor: cleaned up code

--- src/main/java/core/FloorSubsystem.java
+++ src/main/java/core/FloorSubsystem.java
@@ -3,7 +3,9 @@ package core;
 /**
  * @summary Floor subsystem simulates the functionality of a particular floor
  */
+
 public class FloorSubsystem implements Runnable {
+
     private enum ButtonState {
         UP, DOWN, UNPRESSED
     }
@@ -17,47 +19,46 @@ public class FloorSubsystem implements Runnable {
     private ElevatorState elevatorState;
     private final int floor;
 
-    public FloorSubsystem(final Scheduler scheduler, final int number) {
+    public FloorSubsystem(final Scheduler scheduler, final int floor) {
         this.scheduler = scheduler;
         this.buttonState = ButtonState.UNPRESSED;
         this.elevatorState = ElevatorState.NONE;
-        this.number = number;
+        this.floor = floor;
     }
 
-    public void setButtonState(final ButtonState state) {
+    @Override
+    public void run() {
+        this.scheduler.registerFloorSubsystem(this);
+        // ArrayList<FloorData> floorRequests =
+        // FloorReader(this.path).getFloorRequests();
+        boolean running = true;
+        while (running) {
+            /*
+             * FloorData firstFloorRequest = floorRequests.get(0); if(simTime ==
+             * firstFloorRequest.getTime()){ floorRequests.remove(0);
+             * this.scheduler.addFloorEvent(floorData); }
+             */
+        }
+    }
+
+    public void setButtonState(ButtonState state) {
         this.buttonState = state;
     }
-    
-    public ButtonState getButtonState(){
+
+    public ButtonState getButtonState() {
         return this.buttonState;
     }
-    
-    
-    public void setElevatorState(final ElevatorState state) {
+
+    public void setElevatorState(ElevatorState state) {
         this.elevatorState = state;
     }
 
     public ElevatorState getElevatorState() {
         return this.elevatorState;
     }
-    
+
     public int getFloorNumber() {
-        return this.number;
-    }
-    
-    // Run
-    @Override
-    public void run() {
-        this.scheduler.registerFloorSubsystem(this);
-        //ArrayList<FloorData> floorRequests = FloorReader(this.path).getFloorRequests();
-        boolean running = true;
-        while (true) {
-            /*FloorData firstFloorRequest = floorRequests.get(0);
-            if(simTime == firstFloorRequest.getTime()){
-                floorRequests.remove(0);
-                this.scheduler.addFloorEvent(floorData);
-            }*/
-        }
+        return this.floor;
     }
 
 }
