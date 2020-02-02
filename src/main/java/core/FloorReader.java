96e832fa14cb2831b2a47a699d49eb9840c5ad57
Randy Taylor <tehgecKozzz@gmail.com>
2020-02-01 19:33:16 -0500

    refactor: Simplify floor subsystem file reading

--- src/main/java/core/FloorReader.java
+++ src/main/java/core/FloorReader.java
@@ -2,52 +2,43 @@ package core;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+
 import core.FloorData;
 
 import core.FloorData.ButtonState;
 
 /**
- * @summary Floor subsystem to read an input file using the format shown in project specifications.
+ * @summary Reads an input file following the floor subsystem format into a list
+ *          of serializable objects.
  */
 public class FloorReader {
-    // Stores the queue of people requesting to get on the elevator of type FloorData.
-    private final ArrayList<FloorData> floorRequests; 
-    
-    public FloorReader(final String path){ 
-        floorRequests = new ArrayList<FloorData>();
-        try {
-            this.readFile(path);
-        } catch (Exception e) {
+    public ArrayList<FloorData> readFile(final String path) {
+        final ArrayList<FloorData> floorRequests = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(path)))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                final String[] data = line.split(" ");
+                final LocalTime localTime =
+                        LocalTime.parse(data[0], DateTimeFormatter.ofPattern("HH:mm:ss.S"));
+                final int floorNumber = Integer.parseInt(data[1]);
+                final ButtonState buttonState = ButtonState.valueOf(data[2]);
+
+                floorRequests.add(new FloorData(1, floorNumber, buttonState, localTime));
+            }
+        } catch (FileNotFoundException f) {
+            System.err.println(f);
+        } catch (IOException e) {
             System.err.println(e);
         }
+
+        return floorRequests;
     }
-    
-    // Store input file into an ArrayList of type FloorData.
-    private void readFile(final String path) throws Exception {
-        final File file = new File(path);
-        final BufferedReader br = new BufferedReader(new FileReader(file)); 
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.S");
-        String line; 
-        
-        while ((line = br.readLine()) != null) { 
-          final String[] data = line.split(" "); 
-          final LocalTime localTime = LocalTime.parse(data[0], formatter);
-          final int floorNumber = Integer.parseInt(data[1]);
-          final ButtonState buttonState = ButtonState.valueOf(data[2]);
-          
-          final FloorData floorData = new FloorData(1, floorNumber, buttonState, localTime);
-          floorRequests.add(floorData);                    
-        } 
-        br.close();
-    }
-    
-    // Getter to get stored ArrayList of FloorData (represents list of floors in text file).
-    public ArrayList<FloorData> getFloorRequests() {
-        return this.floorRequests;
-    }
-    
 }
