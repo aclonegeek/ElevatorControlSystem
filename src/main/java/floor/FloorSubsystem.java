d42328f91c26cee2c0832304cc1874bac518fa20
Randy Taylor <tehgecKozzz@gmail.com>
2020-02-11 20:25:34 -0500

    code: implement adding floor requests refactor: FloorSubsystem.run() no longer reads a file, it must be done via addFloorRequest()

--- src/main/java/floor/FloorSubsystem.java
+++ src/main/java/floor/FloorSubsystem.java
@@ -5,43 +5,50 @@ import java.util.ArrayList;
 import scheduler.Scheduler;
 
 /**
- * TODO: Update this - it does more than just a particular floor.
- * Simulates the functionality of a particular floor.
- * Communicates with the scheduler through FloorData objects.
+ * TODO: Update this - it does more than just a particular floor. Simulates the
+ * functionality of a particular floor. Communicates with the scheduler through
+ * FloorData objects.
  */
 public class FloorSubsystem implements Runnable {
     private enum ButtonState {
         UP, DOWN, UNPRESSED
     }
 
-    public static int MAX_FLOORS = 50;
+    public static final int MAX_FLOORS = 50;
 
     private final Scheduler scheduler;
     private ButtonState buttonState;
     private final int floor;
     private final FloorReader floorReader;
 
+    private ArrayList<FloorData> requests;
+
     public FloorSubsystem(final Scheduler scheduler, final int floor) {
         this.scheduler = scheduler;
         this.buttonState = ButtonState.UNPRESSED;
         this.floor = floor;
         this.floorReader = new FloorReader();
+        this.requests = new ArrayList<>();
     }
 
     /**
-     * Reads input data from a file and continually tries to send {@link FloorData} events
-     * to the {@link Scheduler}.
+     * Sends {@link FloorData} requests to the {@link Scheduler}.
      */
     @Override
     public void run() {
-        final String filePath = this.getClass().getResource("/floorData.txt").getFile();
-        final ArrayList<FloorData> floorRequests = floorReader.readFile(filePath);
-
         while (true) {
-            if (!floorRequests.isEmpty()) {
-                this.scheduler.addElevatorEvent(floorRequests.remove(0));
+            if (!this.requests.isEmpty()) {
+                this.scheduler.scheduleElevator(this.requests.remove(0));
             }
         }
+    }
+
+    public void addFloorRequest(final FloorData data) {
+        this.requests.add(data);
+    }
+
+    public void addFloorRequest(final String path) {
+        this.requests = this.floorReader.readFile(this.getClass().getResource(path).getFile());
     }
 
     public void setButtonState(final ButtonState state) {
