d551d9ac189f247a42c9e48dc6083dec1d32b5f8
LayneKM <laynemikan@gmail.com>
2020-03-02 17:53:03 -0500

    code: add static array of values to ElevatorAction

--- src/main/java/elevator/ElevatorAction.java
+++ src/main/java/elevator/ElevatorAction.java
@@ -1,5 +1,8 @@
 package elevator;
 
 public enum ElevatorAction {
-    MOVE_UP, MOVE_DOWN, STOP_MOVING, OPEN_DOORS, CLOSE_DOORS, DESTINATION_REACHED
+    MOVE_UP, MOVE_DOWN, STOP_MOVING, OPEN_DOORS, CLOSE_DOORS, DESTINATION_REACHED;
+    
+    // Array representation of the values which can be used to deserialize data.
+    public static final ElevatorAction[] values = ElevatorAction.values();
 }
