6340d0cd2ac8b09fec4536dfe3e257a315f8ee10
LayneKM <laynemikan@gmail.com>
2020-03-02 17:53:45 -0500

    refactor: run formatter

--- src/main/java/elevator/ElevatorAction.java
+++ src/main/java/elevator/ElevatorAction.java
@@ -2,7 +2,7 @@ package elevator;
 
 public enum ElevatorAction {
     MOVE_UP, MOVE_DOWN, STOP_MOVING, OPEN_DOORS, CLOSE_DOORS, DESTINATION_REACHED;
-    
+
     // Array representation of the values which can be used to deserialize data.
     public static final ElevatorAction[] values = ElevatorAction.values();
 }
