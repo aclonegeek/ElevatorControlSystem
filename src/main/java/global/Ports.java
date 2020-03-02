6dd65bbb60bc733328baaa49a6d790ac3d9929a9
LayneKM <laynemikan@gmail.com>
2020-03-02 15:04:14 -0500

    code: add ports

--- src/main/java/global/Ports.java
+++ src/main/java/global/Ports.java
@@ -1,5 +1,7 @@
-package core;
+package global;
 
-public class Config {
-    public static int SCHEDULER_PORT = 69;
+public class Ports {
+    public static final int SCHEDULER = 6969;
+    public static final int ELEVATOR = 6970;
+    public static final int FLOOR = 6971;
 }
