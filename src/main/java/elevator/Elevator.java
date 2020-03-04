85cdfcfaabfdd7faaad91e09ffcc64217aa4b35b
LayneKM <laynemikan@gmail.com>
2020-03-04 16:22:07 -0500

    code: update Elevator to not be runnable, ElevatorSubsystem to be runnable

--- src/main/java/elevator/Elevator.java
+++ src/main/java/elevator/Elevator.java
@@ -2,52 +2,43 @@ package elevator;
 
 import global.Globals;
 
-public class Elevator implements Runnable {
+public class Elevator {
     private final ElevatorSubsystem elevatorSubsystem;
     private final ElevatorSystem elevatorSystem;
 
     public Elevator(final int elevatorId, final ElevatorSystem elevatorSystem) {
         this.elevatorSystem = elevatorSystem;
-        this.elevatorSubsystem = new ElevatorSubsystem(elevatorId);
-    }
-
-    @Override
-    public void run() {
-        // TODO: Need to put something here so it doesn't terminate.
+        this.elevatorSubsystem = new ElevatorSubsystem(elevatorId, elevatorSystem);
+        new Thread(elevatorSubsystem).start();
     }
 
     /*
-     * Parse data, update ElevatorSystem state, then return a response once it's complete.
-     * receiveData[0] is the id of the elevator.
+     * Parse data and update ElevatorSystem state:
+     * receiveData[0] is the id of the Elevator.
      * receiveData[1] is the serialized ElevatorAction.
-     * receiveData[2+] is the destination floor.
+     * receiveData[2] is the current floor.
+     * 
+     * Then return a response:
+     * sendData[0] signifies the data is from an Elevator.
+     * sendData[1] is the id of the Elevator.
+     * sendData[2] is the serialized ElevatorResponse.
+     * sendData[3] is the current floor.
      */
     public void processData(byte[] receiveData) {
         // Parse action and destination floor from receiveData.
-        final ElevatorAction action = ElevatorAction.values[receiveData[2]];
-        String destinationFloorString = new String();
-        for (int i = 2; i < receiveData.length; i++) {
-            destinationFloorString += receiveData[i];
-        }
-        final int destinationFloor = Integer.parseInt(destinationFloorString);
+        final ElevatorAction action = ElevatorAction.values[receiveData[1]];
+        final int destinationFloor = (int) receiveData[2];
         
         // Update ElevatorSubsystem state.
         final ElevatorResponse response = this.elevatorSubsystem.updateState(action, destinationFloor);
         
         // Construct response message.
         final int currentFloor = elevatorSubsystem.getCurrentFloor();
-        final byte[] sendData = new byte[currentFloor > 9 ? 5 : 4];
+        final byte[] sendData = new byte[4];
         sendData[0] = Globals.FROM_ELEVATOR;
         sendData[1] = (byte) elevatorSubsystem.getElevatorId();
         sendData[2] = (byte) response.ordinal();
-
-        if (currentFloor > 9) {
-            String currentFloorString = Integer.toString(currentFloor);
-            sendData[3] = (byte) Character.getNumericValue((currentFloorString.charAt(0)));
-            sendData[4] = (byte) Character.getNumericValue((currentFloorString.charAt(1)));
-        } else {
-            sendData[3] = (byte) currentFloor;
-        }
+        sendData[3] = (byte) currentFloor;
         
         // Send response message.
         this.elevatorSystem.sendData(sendData);
--- src/main/java/elevator/ElevatorSubsystem.java
+++ src/main/java/elevator/ElevatorSubsystem.java
@@ -2,15 +2,37 @@ package elevator;
 
 import java.time.LocalTime;
 
-public class ElevatorSubsystem {
+import global.Globals;
+
+public class ElevatorSubsystem implements Runnable {
     private final int elevatorId;
+    private final ElevatorSystem elevatorSystem;
     private int currentFloor;
     private ElevatorState state;
 
-    public ElevatorSubsystem(final int elevatorId) {
+    public ElevatorSubsystem(final int elevatorId, final ElevatorSystem elevatorSystem) {
         this.elevatorId = elevatorId;
+        this.elevatorSystem = elevatorSystem;
         this.currentFloor = 0;
         this.state = ElevatorState.IDLE_DOOR_OPEN;
+    }
+
+    public void run() {
+        while (true) {
+            if (this.state == ElevatorState.MOVING_UP) {
+                currentFloor++;
+                this.sendCurrentFloor();
+            } else if (this.state == ElevatorState.MOVING_DOWN) {
+                currentFloor--;
+                this.sendCurrentFloor();
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
     }
 
     // TODO: Set elevator lights based on selected destinationFloor.
@@ -20,11 +42,9 @@ public class ElevatorSubsystem {
             return ElevatorResponse.DESTINATION_REACHED;
         case MOVE_UP:
             this.state = ElevatorState.MOVING_UP;
-            currentFloor++;
             break;
         case MOVE_DOWN:
             this.state = ElevatorState.MOVING_DOWN;
-            currentFloor--;
             break;
         case STOP_MOVING:
             this.state = ElevatorState.IDLE_DOOR_CLOSED;
@@ -55,5 +75,20 @@ public class ElevatorSubsystem {
 
     public ElevatorData getElevatorData() {
         return new ElevatorData(this.elevatorId, this.currentFloor, LocalTime.now());
+    }
+
+    /*
+     * Send the elevator's updated currentFloor to the Scheduler:
+     * sendData[0] signifies the data is from an Elevator.
+     * sendData[1] is the id of the Elevator.
+     * sendData[2] is the current floor.
+     */
+    public void sendCurrentFloor() {
+        final byte[] sendData = new byte[3];
+        sendData[0] = Globals.FROM_ELEVATOR;
+        sendData[1] = (byte) this.elevatorId;
+        sendData[2] = (byte) this.currentFloor;
+        
+        this.elevatorSystem.sendData(sendData);
     }
 }
