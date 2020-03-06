5d562ef12df22510b6d1a43526998c173e45d3a2
LayneKM <laynemikan@gmail.com>
2020-03-06 10:55:16 -0500

    refactor: add documentation

--- src/main/java/elevator/ArrivalSensor.java
+++ src/main/java/elevator/ArrivalSensor.java
@@ -12,6 +12,11 @@ import global.Globals;
  * Continually checks to see if the Elevator is in range.
  * If so, sends data to the Scheduler to check whether it should stop at this floor.
  * If the Elevator is scheduled to stop here, the Scheduler will notify the ElevatorSystem.
+ * 
+ * sendData format is as follows:
+ * sendData[0] signifies the data is from an ArrivalSensor.
+ * sendData[1] is the id of the Elevator.
+ * sendData[2] is the floor.
  */
 public class ArrivalSensor implements Runnable {
     private final int floor;
