f0e2c89cacdbd9be6ee12eff6061f87c1c1b986f
LayneKM <laynemikan@gmail.com>
2020-03-06 10:51:39 -0500

    code: update ArrivalSensor to send data to Scheduler

--- src/main/java/elevator/ArrivalSensor.java
+++ src/main/java/elevator/ArrivalSensor.java
@@ -1,5 +1,11 @@
 package elevator;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
+
 import global.Globals;
 
 /*
@@ -10,25 +16,51 @@ import global.Globals;
 public class ArrivalSensor implements Runnable {
     private final int floor;
     private final ElevatorSubsystem elevator;
-    int lastDetectedFloor;
+    private DatagramSocket sendSocket;
 
     public ArrivalSensor(final int floor, final ElevatorSubsystem elevator) {
         this.floor = floor;
         this.elevator = elevator;
-        this.lastDetectedFloor = 0;
+
+        try {
+            this.sendSocket = new DatagramSocket();
+        } catch (SocketException se) {
+            System.err.println(se);
+            System.exit(1);
+        }
     }
 
     @Override
     public void run() {
+        int lastDetectedFloor = 0;
+
+        // TODO: Remove sysouts because they'll spam the ElevatorSystem console.
         while (true) {
             final int elevatorFloor = elevator.getCurrentHeight() / Globals.FLOOR_HEIGHT;
             if (elevatorFloor == floor && elevatorFloor != lastDetectedFloor) {
-                System.out.println("Floor " + floor + " has detected elevator " + elevator.getElevatorId());
-                this.lastDetectedFloor = floor;
-                
-                // TODO: Send data to Scheduler to see if the Elevator should stop here.
+                System.out.println("Floor " + floor + " arrival sensor has detected elevator: "
+                        + elevator.getElevatorId());
+                lastDetectedFloor = floor;
+
+                // Send data to Scheduler. If the Elevator should stop at this floor, the
+                // Scheduler will then notify the ElevatorSystem.
+                final byte[] sendData = new byte[3];
+                sendData[0] = Globals.FROM_ARRIVAL_SENSOR;
+                sendData[1] = (byte) elevator.getElevatorId();
+                sendData[2] = (byte) floor;
+                final DatagramPacket sendPacket =
+                        new DatagramPacket(sendData, sendData.length, Globals.IP, Globals.SCHEDULER_PORT);
+
+                System.out.println("ArrivalSensor sending to port " + sendPacket.getPort() + ": "
+                        + Arrays.toString(sendData));
+                try {
+                    this.sendSocket.send(sendPacket);
+                } catch (IOException e) {
+                    System.err.println(e);
+                    System.exit(1);
+                }
             }
-            
+
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
