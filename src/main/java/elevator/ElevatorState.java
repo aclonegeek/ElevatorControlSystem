bc238453f1b184bfa3cb828ac572badaf92f05a6
Randy Taylor <tehgecKozzz@gmail.com>
2020-03-06 20:21:03 -0500

    code: add MOVING_DOOR_CLOSED to ElevatorState

--- src/main/java/elevator/ElevatorState.java
+++ src/main/java/elevator/ElevatorState.java
@@ -1,7 +1,7 @@
 package elevator;
 
 public enum ElevatorState {
-    IDLE_DOOR_OPEN, IDLE_DOOR_CLOSED, MOVING_UP, MOVING_DOWN;
+    IDLE_DOOR_OPEN, IDLE_DOOR_CLOSED, MOVING_DOOR_CLOSED, MOVING_UP, MOVING_DOWN;
 
     public static final ElevatorState[] values = ElevatorState.values();
 }
