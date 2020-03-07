ca59dde1d717b466731e7ef4c41b129fc43447a9
Randy Taylor <tehgecKozzz@gmail.com>
2020-03-07 13:31:43 -0500

    refactor: only send STATE_CHANGED when state actually changes

--- src/main/java/elevator/Elevator.java
+++ src/main/java/elevator/Elevator.java
@@ -29,29 +29,28 @@ public class Elevator {
      * receiveData[0] signifies the data is from the Scheduler.
      * receiveData[1] is the id of the elevator.
      * receiveData[2] is the serialized ElevatorAction.
-     * receiveData[3] is the destination floor (used to set the button state).
      *
      * Then return a response:
      * sendData[0] signifies the data is from an Elevator.
      * sendData[1] is the id of the Elevator.
-     * sendData[2] is the serialized ElevatorResponse.
+     * sendData[3] is the serialized ElevatorState.
+     * sendData[4] is the serialized ElevatorResponse.
      */
     public void processData(final byte[] receiveData) {
-        // Parse action and destination floor from receiveData.
         final ElevatorAction action = ElevatorAction.values[receiveData[2]];
-
-        // Update ElevatorSubsystem state.
+        final ElevatorState previousState = this.elevatorSubsystem.getState();
         final ElevatorResponse response = this.elevatorSubsystem.updateState(action);
 
-        // Construct response message.
-        final byte[] sendData = new byte[4];
-        sendData[0] = Globals.FROM_ELEVATOR;
-        sendData[1] = (byte) elevatorSubsystem.getElevatorId();
-        sendData[2] = (byte) Request.STATE_CHANGED.ordinal();
-        sendData[3] = (byte) response.ordinal();
+        if (previousState != this.elevatorSubsystem.getState()) {
+            final byte[] sendData = new byte[5];
+            sendData[0] = Globals.FROM_ELEVATOR;
+            sendData[1] = (byte) elevatorSubsystem.getElevatorId();
+            sendData[2] = (byte) Request.STATE_CHANGED.ordinal();
+            sendData[3] = (byte) this.elevatorSubsystem.getState().ordinal();
+            sendData[4] = (byte) response.ordinal();
 
-        // Send response message.
-        this.elevatorSystem.sendData(sendData);
+            this.elevatorSystem.sendData(sendData);
+        }
     }
 
     public ElevatorSubsystem getSubsystem() {
