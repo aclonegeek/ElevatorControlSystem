480f0f5bfb779d1a0069136d8df313d5756641e3
LayneKM <laynemikan@gmail.com>
2020-04-04 22:29:07 -0400

    refactor: add WAITING state and invalid sending of READY

--- src/main/java/elevator/ElevatorState.java
+++ src/main/java/elevator/ElevatorState.java
@@ -1,7 +1,7 @@
 package elevator;
 
 public enum ElevatorState {
-    IDLE_DOOR_OPEN, DOOR_CLOSED_FOR_IDLING, DOOR_CLOSED_FOR_MOVING, MOVING_UP, MOVING_DOWN;
+    IDLE_DOOR_OPEN, DOOR_CLOSED_FOR_IDLING, DOOR_CLOSED_FOR_MOVING, MOVING_UP, MOVING_DOWN, WAITING;
 
     public static final ElevatorState[] values = ElevatorState.values();
 }
